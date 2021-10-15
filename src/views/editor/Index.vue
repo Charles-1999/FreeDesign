26f46e784d3a081fea3f1659fdb1f6c0b344c514
charlesjylu <charlesjylu@tencent.com>
2021-10-15 15:49:07 +0800

    feat: 重构编辑器

---
+++ src/views/editor/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Canvas />
+</template>
+
+<script>
+import Canvas from './components/canvas/Canvas.vue';
+
+export default {
+  name: 'Editor',
+
+  components: {
+    Canvas
+  }
+};
+</script>
---
+++ src/views/editor/components/canvas/Canvas.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="canvas-wrapper"
+    :style="{
+      transform: 'scale(' + data.scale + ')',
+      width: data.width + 'px',
+      height: data.height + 'px'
+    }">
+      <lib-text text="123" />
+      <!-- <component
+        :is="1" /> -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Canvas',
+
+  data() {
+    return {
+      data: {
+        width: 375,
+        height: 650,
+        scale: 1
+      }
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+  .canvas-wrapper {
+    box-shadow: 0 3px 10px #dcdcdc;
+    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZAgMAAAC5h23wAAAAAXNSR0IB2cksfwAAAAlQTFRF9fX18PDwAAAABQ8/pgAAAAN0Uk5T/yIA41y2EwAAABhJREFUeJxjYIAC0VAQcGCQWgUCDUONBgDH8Fwzu33LswAAAABJRU5ErkJggg==);
+  }
+</style>
