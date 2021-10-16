e838d296059ed8d2d583b3326f52d75ae31cae95
charlesjylu <charlesjylu@tencent.com>
2021-10-17 01:18:19 +0800

    feat: 封装lib-text

--- src/components/lib/Text/Text.vue
+++ src/components/lib/Text/Text.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <span>{{text}}</span>
+    <span
+      ref="span"
+      @dblclick="handleDbClick"
+      @blur="handleChange">
+      {{ innerText }}
+    </span>
   </div>
 </template>
 
@@ -13,6 +18,35 @@ export default {
       type: String,
       required: true,
       default: ''
+    }
+  },
+
+  data() {
+    return {
+      innerText: this.text
+    };
+  },
+
+  methods: {
+    /**
+     * 处理双击事件
+     * @return {void}
+     */
+    handleDbClick() {
+      // 双击编辑
+      this.$refs.span.contentEditable = true;
+      this.$refs.span.focus();
+    },
+
+    /**
+     * 处理文本change事件
+     * @return {void}
+     */
+    handleChange() {
+      const text = this.$refs.span.innerHTML;
+
+      this.innerText = text;
+      this.$emit('change', text);
     }
   }
 };
