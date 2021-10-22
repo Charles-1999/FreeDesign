e5cf7ac6c4035bb0956ad2367911e71f6025dda1
charlesjylu <charlesjylu@tencent.com>
2021-10-22 16:36:46 +0800

    feat: 修改Text组件

---
+++ src/components/lib/Text/config.js
@@ -0,0 +1,18 @@
+/**
+ * LibText组件配置
+ */
+
+// 默认样式
+export const defaultStyle = {
+  color: 'pink'
+};
+
+// 默认参数
+export const defaultProps = {
+  text: '这是一段文字'
+};
+
+export default {
+  defaultStyle,
+  defaultProps
+};
