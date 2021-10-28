66b8958e67c17154d8261e4c346c055ed35e9ce3
charlesjylu <charlesjylu@tencent.com>
2021-10-28 23:47:47 +0800

    feat: 全局注册公共组件

--- src/main.js
+++ src/main.js
@@ -15,7 +15,12 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 // lib components
 import libComponents from './components/lib';
+// common components
+import commonComponents from './components/common';
+
 Vue.use(libComponents);
+Vue.use(commonComponents);
+
 Vue.use(ElementUI);
 
 const pagesRouters = require.context(
