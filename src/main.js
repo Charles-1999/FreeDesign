fc8b5afd1c0d0ccf6b8970492254db36e10281d9
charlesjylu <charlesjylu@tencent.com>
2021-10-30 12:13:51 +0800

    feat: 导入全局样式

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-/*
- * @Author: your name
- * @Date: 2021-10-21 16:14:50
- * @LastEditTime: 2021-10-26 10:53:37
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /FreeDesign/src/main.ts
- */
 import Vue from 'vue';
 import App from './App.vue';
+
 import routerFn from './router';
+
 import store from './store';
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+// styles
+import './assets/styles/index.less';
 
 // lib components
 import libComponents from './components/lib';
