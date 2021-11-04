4c34f832ce593afedda0dda2dce403cbfee297ed
charlesjylu <charlesjylu@tencent.com>
2021-11-04 12:07:12 +0800

    feat: 全局默认配置

--- config/index.js
+++ config/index.js
@@ -1,12 +1,23 @@
 import devConfig from './config.dev';
 import prodConfig from './config.prod';
 
+// 默认配置
+const defaultConfig = {
+  // cos平台 七牛云
+  cos: {
+    uploadUrl: 'https://upload-z2.qiniup.com',
+    queryUrl: 'http://r1zff9ii3.hn-bkt.clouddn.com/'
+  }
+};
+
 const config = {
   dev: {
+    ...defaultConfig,
     ...devConfig
   },
 
   prod: {
+    ...defaultConfig,
     ...prodConfig
   }
 };
