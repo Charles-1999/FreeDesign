88878ac5ad62a64a3eaa80d56e8658f4bf06780f
charlesjylu <charlesjylu@tencent.com>
2021-11-09 22:01:26 +0800

    feat: 添加icon组件

---
+++ src/components/common/icon/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    class="iconfont"
+    :style="{
+      fontSize: `${size}px`
+    }"
+    aria-hidden="true">
+    <use :xlink:href="`#${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: ''
+    },
+
+    size: {
+      type: Number,
+      required: false,
+      default: 14
+    }
+  }
+};
+</script>
