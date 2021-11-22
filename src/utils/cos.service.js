f528b3f3133f9c269a33408f89dd9b1a41ce11af
charlesjylu <charlesjylu@tencent.com>
2021-11-22 15:34:20 +0800

    feat: cos封装

--- src/utils/cos.service.js
+++ src/utils/cos.service.js
@@ -3,6 +3,7 @@
  */
 
 import http from '@utils/http.service';
+import config from '@config';
 
 /**
  * 获取token 并 存在storage
@@ -12,6 +13,18 @@ export const getUploadToken = async () => {
   const { token } = await http.get('/resources/qiniu/upload_token');
 
   return token;
+};
+
+/**
+ * 上传七牛
+ */
+export const upload = (formData) => {
+  return http.post(config.cos.uploadUrl, formData, {
+    withCredentials: false,
+    selfHttpConfig: {
+      external: true
+    }
+  });
 };
 
 export default {
